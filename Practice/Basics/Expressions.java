ab6372ec7c1430db52bc1fd841207301e20e3c37
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:59:29 +0500

    Add files via upload

---
+++ Practice/Basics/Expressions.java
@@ -0,0 +1,30 @@
+ public class Expressions //class header
+
+{ //class body starts
+
+	public static void main(String [] lab3task3) //main header
+
+	{ //main body starts
+
+		//variables
+		int exp1=7+2*3-3;
+		int exp2=(5+10)*3;
+		int exp3=4*2*(5+9)/2;
+		int exp4=14*7%2-5;
+		int exp5=3+7*(8-4)/2;
+		int exp6=2*6/6*5*5*3;
+		int exp7=12+2*(19-5);
+
+		//print statements
+		System.out.println(" 7+2*3-3     = " + exp1);
+		System.out.println("(5+10)*3     = " + exp2);
+		System.out.println(" 4*2*(5+9)/2 = " + exp3);
+		System.out.println(" 14*7%2-5    = " + exp4);
+		System.out.println(" 3+7*(8-4)/2 = " + exp5);
+		System.out.println(" 2*6/6*5*5*3 = " + exp6);
+		System.out.print(" 12+2*(19-5) = " + exp7);
+
+
+	} //main body ends
+
+} //class body ends
