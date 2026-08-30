0c87cbdc0c60a1b0df6aa24bc36d4551ca9ceefd
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:46:50 +0500

    Add files via upload

---
+++ Practice/Basics/Averages.java
@@ -0,0 +1,24 @@
+public class Averages //class header
+
+{ //class body starts
+
+	public static void main(String [] lab3task1) //main header
+
+	{ //main body starts
+
+		//Variables
+		int number1=11;
+		int number2=26;
+		int number3=18;
+
+		int sumIntegers=number1+number2+number3;
+		int averageIntegers=sumIntegers/3;
+
+		//Print statements
+
+		System.out.println("Fist Number: " + number1 + "\nSecond Number: " + number2 + "\nThird Number: " + number3);
+		System.out.println("Sum of numbers: " + sumIntegers + "\nAverage of numbers: " + averageIntegers);		
+
+	} //main body ends
+
+} //class body ends
