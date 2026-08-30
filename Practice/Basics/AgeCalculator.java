22569b7585a7ba6737538fd7e4c7b9a34c5a72b5
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 22:08:09 +0500

    Add files via upload

---
+++ Practice/Basics/AgeCalculator.java
@@ -0,0 +1,37 @@
+/*Creating first assignment
+  Task number 2
+  Age Calculator*/
+
+public class AgeCalculator  //Class Header
+
+{  //Class body starts
+
+	public static void main(String [] args)  //Main Header
+
+	{  //Main body starts
+
+		//Variables
+
+		int birthYear =2005;
+		int currentyear =2025;
+		int age =2025-2005;
+
+		//print Statements
+
+		System.out.println("\n===============================");
+
+		System.out.println("\t Age Calculator");
+
+		System.out.println("===============================");
+
+		System.out.println("Birth Year: \t " + birthYear);
+		System.out.println("Current Year: \t " + currentyear);
+
+		System.out.println("-------------------------------");
+
+		System.out.println("Your Age is: \t " + age + " years");
+
+		System.out.print("===============================");
+
+	}  //Main body ends
+}  //Class body ends
