9eb0bb85a477ba6d8acf7ce1cd74fe48cda6bf64
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:31:52 +0500

    Add files via upload
    My first Java Code

---
+++ Practice/Basics/FirstPractice.java
@@ -0,0 +1,9 @@
+/*This is my first java program practice
+  I learned Skeleton
+  I practiced comments, print and println statements, and escape sequences in this program*/
+public class FirstPractice{	//class header
+	public static void main (String args[]){	//main method header+class body
+		System.out.print("Assalam O Alikum Wrhmtullah He Wbrkt \t How Are You?");	//print statement
+		System.out.println("How Was Your Day? \r Nice to Meet You. \n Best of Luck!");
+	}//main method body ends
+}//class body ends
