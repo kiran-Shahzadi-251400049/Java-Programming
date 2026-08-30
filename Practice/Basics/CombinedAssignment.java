13c3c31ac3c9b05db32cd46f95a866268679d877
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:50:38 +0500

    Add files via upload

---
+++ Practice/Basics/CombinedAssignment.java
@@ -0,0 +1,26 @@
+public class CombinedAssignment //class header
+
+{ //class body starts
+
+	public static void main(String [] lab3task5) //main header
+
+	{ //main body starts
+
+		//variables
+		int count=10;
+		System.out.println("Count = " + count);
+		count +=5;
+		System.out.println("Increment count by 5 = " + count);
+		count -=2;
+		System.out.println("Decrement count by 2 = " + count);
+		count *=3;
+		System.out.println("Multiply count by 3 = " + count);
+		count /=2;
+		System.out.println("Divide count by 2 = " + count);
+		count %=4;
+		System.out.println("Remainder when count is divided by 4 = " + count);
+
+
+	} //main body ends
+
+} //class body ends
