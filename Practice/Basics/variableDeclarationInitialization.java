697c75bd71deafbbf86cb4d290cb493cd569f2e4
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:40:55 +0500

    Add files via upload

---
+++ Practice/Basics/variableDeclarationInitialization.java
@@ -0,0 +1,18 @@
+public class variableDeclarationInitialization 
+{
+
+	public static void main (String [] args)
+	{
+		//declaration + initialization
+
+		double number= 200.02;
+		double percentage= number*0.25;
+
+		//print statement
+
+		System.out.println("The number is: "+ number);
+		System.out.print("25% of number is: "+ percentage);
+	}
+}
+
+
