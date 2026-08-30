2d0b7943c97bcd5bc9cf4bd57b330dc34eed05f7
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:54:18 +0500

    Add files via upload

---
+++ Practice/Basics/Divisions.java
@@ -0,0 +1,30 @@
+public class Divisions //class header
+
+{ //class body starts
+
+	public static void main(String [] lab3task4) //main header
+
+	{ //main body starts
+
+		//variables
+		double exp1=14/2;
+		double exp2=15/3;
+		double exp3=19%4;
+		double exp4=25/5.0;
+		double exp5=12.0/3.0;
+		double exp6=1/9;
+		double exp7=15.0%3.0;
+
+		//print statements
+		System.out.println("14/2 = " + exp1);
+		System.out.println("15/3 = " + exp2);
+		System.out.println("19%4 = " + exp3);
+		System.out.println("25/5.0 = " + exp4);
+		System.out.println("12.0/3.0 = " + exp5);
+		System.out.println("1/9 = " + exp6);
+		System.out.print("15.0%3.0 = " + exp7);
+
+
+	} //main body ends
+
+} //class body ends
