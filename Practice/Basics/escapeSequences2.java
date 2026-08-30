2c5c5d4311fd0bc887eb8de534a43d0ab2ae5e0f
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:43:08 +0500

    Add files via upload

---
+++ Practice/Basics/escapeSequences2.java
@@ -0,0 +1,13 @@
+public class escapeSequences2 //class header
+
+{ //class body starts
+
+	public static void main(String [] lab2task2) //main header
+
+	{ //main body starts
+
+		System.out.println("Gift University offers different types of scholarships: \n1. Result Based Scholarships and Awards \n \tQuaid-e-Azam Scholarship \n \t Fatima Jinnah Scholarship \n \t Merit and Industry Scholarship ");
+		System.out.print("\n \n 2. Scholarship for the Community \n \t Orphan and Disabled Students \n \t Married Couple");
+	} //main body ends
+
+} //class body end
