e45f3546c357bddea98367930593e9d5b97636ec
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:36:35 +0500

    Add files via upload

---
+++ Practice/Basics/escapeSequences.java
@@ -0,0 +1,13 @@
+public class escapeSequences //class header
+
+{ //class body starts
+
+	public static void main(String [] lab2task3)  //main header
+
+	{ //main body starts
+
+		System.out.println("I am a little teapot \n \t Short and stout \n \n \t Here is my handle \n \t \t Here is my spout. \n \n \t When I get all 'steamed up' \n Hear me shout \n \n \t \"Tip me over \n \t and pour me out!\".  ");
+
+	} //main body ends
+
+} //class body ends
