a2e5801b2e1921dffef67cd0b7d5a45f34e9d5cb
Kiran Shahzadi <kiran1712shahzadi@gmail.com>
2026-08-30 16:56:36 +0500

    Add files via upload

---
+++ Practice/Basics/ENotation.java
@@ -0,0 +1,23 @@
+public class ENotation //class header
+
+{  //class body starts
+
+    public static void main(String[] lab3task7) //main header
+	
+	{  //main body starts
+
+	// Step 1: Store numbers in E-notation
+	double numA = 5.823E2;
+	double numB = -5.5214E15;
+	double numC = 6.52896E-2;
+	double numD = -9.1485E-7;
+
+	// Step 2: Print values
+	System.out.println(" 5.823   *  10^2  in E-notation: " + numA);
+	System.out.println("-5.5214  *  10^15 in E-notation: " + numB);
+	System.out.println(" 6.52896 *  10^-2 in E-notation: " + numC);
+	System.out.println("-9.1485  *  10^-7 in E-notation: " + numD);
+
+	}  //main body ends
+
+}  //class body ends
